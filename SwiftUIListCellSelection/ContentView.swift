6ee98a431e375890811a40376250f5c2c69197c7
slava bily <slavabily@me.com>
2021-07-21 13:32:12 +0300

    selection of one either cell is implemented

--- SwiftUIListCellSelection/ContentView.swift
+++ SwiftUIListCellSelection/ContentView.swift
@@ -7,10 +7,50 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Claim: Equatable {
+    var id: Int
+    var description: String
+}
+
+struct ClaimCell: View {
+    
+    var claim: Claim
+    @Binding var selectedClaim: Claim?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            claim == selectedClaim ? Color.green : Color.white
+            Text(claim.description)
+                .padding()
+        }.onTapGesture {
+            self.selectedClaim = self.claim
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    @State var selectedClaim: Claim?
+    
+    var claims = [Claim(id: 0, description: "Value 1"),
+                  Claim(id: 1, description: "Value 2"),
+                  Claim(id: 2, description: "Value 3")]
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            List(claims, id: \.id) { claim in
+                ClaimCell(claim: claim, selectedClaim: self.$selectedClaim)
+            }
+                .padding()
+                .frame(maxHeight:180)
+                .background(Color.gray)
+                .cornerRadius(8)
+            Text("Use : \(self.selectedClaim?.description ?? "none")").onTapGesture {
+                print("Do something (eg go to detail view) with \(self.selectedClaim?.description ?? "none")")
+            }.padding()
+            Spacer()
+        }
     }
 }
 
